191c76458eba8c23790f7500e96707750d085d6c
alebastr12 <thegrace12@gmail.com>
2019-01-25 18:23:47 +0300

    Сделал задание с вводом имени, фамилии, города в центре консоли

---
+++ lesson1/hello/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+б) *Сделать задание, только вывод организовать в центре экрана.
+в) **Сделать задание б с использованием собственных методов (например, Print(string ms, int x,int y).*Создать асс с методами, которые могут пригодиться в вашей учебе (Print, Pause).
+Бастраков Александр
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintPos("Бастраков Александр, Йошкар-Ола", (Console.WindowWidth/2) - 15, Console.WindowHeight/2);
+            Console.ReadKey();
+        }
+        static void PrintPos(string ms, int x, int y) {
+            Console.SetCursorPosition(x, y);
+            Console.Write(ms);
+        }
+    }
+}
