bb934f0e7f091dd079f814d0af7c93a72d14a5d9
alebastr12 <thegrace12@gmail.com>
2019-01-28 22:45:16 +0300

    Сделал программу подсчета хороших чисел в диапазоне от 1 до 1000000000

---
+++ lessons2/goodNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace goodNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime start = DateTime.Now;
+            uint nGood = 0;
+            for (uint i = 1; i <= 1000000000; i++) {
+                if (i % sumOfNum(i) == 0)
+                {
+                    nGood++;
+                }
+            }
+            Console.WriteLine($"Хороших чисел в диапазоне от 1 до 1000000000: {nGood}");
+            Console.WriteLine($"время выполнения: {DateTime.Now-start}");
+            Console.ReadKey();
+        }
+        static uint sumOfNum(uint num)
+        {
+            uint sum = 0;
+            while (num > 0)
+            {
+                sum = sum + num % 10;
+                num = num / 10;
+            }
+            return sum;
+        }
+    }
+}
