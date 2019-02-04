cdf4f60a1b9fd62249ca3fa715dba7e199133700
alebastr12 <thegrace12@gmail.com>
2019-02-05 00:34:09 +0300

    Подпись

--- tworankarray/testLib/Program.cs
+++ tworankarray/testLib/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*
+ Программа для теста класса двумерного массива
+ Бастраков Александр
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
